8c91cf748a9984c87240c8890ee6d10e8de0b660
lwieckowski <lwieckowski@hotmail.com>
2024-05-26 11:00:10 +0200

    Add remap to test current file with pytest

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -9,3 +9,5 @@ vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<leader>t", ":!pytest %")
